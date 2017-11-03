b82dd36472bbb07120b5e529465fe71b95690774
Markus Gage <markgage86@gmail.com>
2017-11-03 15:04:05 -0400

    initial framework setup

---
+++ BayesClassifier/BayesClassifier/BayesClassifier.h
@@ -0,0 +1,19 @@
+//
+//  BayesClassifier.h
+//  BayesClassifier
+//
+//  Created by Markus Gage on 2017-11-03.
+//  Copyright © 2017 Mark Gage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BayesClassifier.
+FOUNDATION_EXPORT double BayesClassifierVersionNumber;
+
+//! Project version string for BayesClassifier.
+FOUNDATION_EXPORT const unsigned char BayesClassifierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BayesClassifier/PublicHeader.h>
+
+
